08360334186adf50905300f358bf110b84bae4fd
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-12 12:20:25 +0800

    updated workflow to get only modified files update

--- src/Controller/Default123Controller.php
+++ src/Controller/Default123Controller.php
@@ -23,7 +23,7 @@ class Default123Controller extends AbstractController
     }
 
     public function test_sniffer(){
-        $arraay = ['sample1' => 'val1', 'sample2' => 'val2'];
+        $arraay = ['sample1' => 'val1', 'sample2' => 'val2aaa'];
         echo 'yes';
     }
 }
