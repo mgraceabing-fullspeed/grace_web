2a126a5104d182bfdf35093b5de2ef9205730cb6
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-12 14:54:14 +0800

    test to exclude deleted files

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -20,7 +20,7 @@ class DefaultController extends AbstractController
         return $this->render(
             'default/index.html.twig',
             [
-                'title' => 'sample title',
+                'title' => 'sample title'
             ]
         );
     }
