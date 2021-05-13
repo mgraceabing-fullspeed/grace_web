f9f7ca07ad9462f44c1b4ea934c4d3c9a9db1dfb
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-13 18:34:47 +0800

    delimeted args 2

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -27,6 +27,6 @@ class DefaultController extends AbstractController
 
     public function FunctionName($value='')
     {
-        echo 'test-yes-aaaaaazaaaaazzaaaaaaaaaaaasssaaaaaa';
+        echo 'test-yes-aaaaaazaaaaazzaaaaaassaaaaaasssaaaaaa';
     }
 }
