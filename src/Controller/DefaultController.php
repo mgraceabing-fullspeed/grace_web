5d5ab56ad2124bd3271c639fa73874e85425e417
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-13 13:40:06 +0800

    new step

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -27,6 +27,6 @@ class DefaultController extends AbstractController
 
     public function FunctionName($value='')
     {
-        echo 'test-yes-aaaaazaaaaazzaaa';
+        echo 'test-yes-aaaaazaaaaazzaaaaa';
     }
 }
