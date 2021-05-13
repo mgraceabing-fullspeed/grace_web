b1cbf36f2afa65a49a6e6712ee0365149796a044
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-13 18:21:35 +0800

    update on args

--- src/Controller/DefaultDDDDController.php
+++ src/Controller/DefaultDDDDController.php
@@ -27,6 +27,6 @@ class DefaultDDDDController extends AbstractController
 
     public function FunctionName($value='')
     {
-        echo 'test-yes-aaaaaaazzzaaaaazzaaaaaaaaaaaa';
+        echo 'test-yes-aaaaaaazzaazaaaaazzaaaaaaaaaaaa';
     }
 }
