3dde07c3fe78bbd23f327f4c62c0dcf998a3ddc0
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-15 22:00:13 +0800

    updated xargs1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -27,6 +27,6 @@ class DefaultController extends AbstractController
 
     public function FunctionName($value='')
     {
-        echo 'test-yesaaa';
+        echo 'test-yesaaas';
     }
 }
--- src/Controller/DefaultDDDDController.php
+++ src/Controller/DefaultDDDDController.php
@@ -27,6 +27,6 @@ class DefaultDDDDController extends AbstractController
 
     public function FunctionName($value='')
     {
-        echo 'test-yesaa';
+        echo 'test-yesaas';
     }
 }
