c7e227b8783e4f874fead91f0ff3dfcdadbae3d0
Mary Grace Abing <marygrace.abing@fullspeedtechnologies.com>
2021-05-15 23:06:01 +0800

    3rd commit

--- src/Controller/DefaultWWWController.php
+++ src/Controller/DefaultWWWController.php
@@ -27,6 +27,6 @@ class DefaultWWWController extends AbstractController
 
     public function FunctionName($value='')
     {
-        echo 'test-yesaaaszasddaaaaaaaaa';
+        echo 'test-yesaaaszasddaaaaaaaaass';
     }
 }
